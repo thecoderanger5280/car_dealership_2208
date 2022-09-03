560abcab90051f86f4618b2cde5fdbc5ea2cecc8
Ryan Loughry <thecoderanger5280@icloud.com>
2022-09-03 16:29:46 -0600

    add paint method

--- lib/car.rb
+++ lib/car.rb
@@ -2,16 +2,21 @@ class Car
   attr_reader :make,
               :model,
               :monthly_payment,
-              :loan_length
+              :loan_length,
+              :color
   def initialize(name, monthly_payment, loan_length)
     @make = name.split(' ')[0]
     @model = name.split(' ')[1]
     @monthly_payment = monthly_payment
     @loan_length = loan_length
-    @color = :red
+    @color = nil
   end
 
   def total_cost
     @monthly_payment * @loan_length
+  end
+
+  def paint!(color)
+    @color = color
   end
 end
