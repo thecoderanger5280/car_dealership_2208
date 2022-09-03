1d272e2493f517affb9c646248b516beacd0c2fa
Ryan Loughry <thecoderanger5280@icloud.com>
2022-09-03 17:12:17 -0600

    implement code to list details of dealership

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -3,19 +3,22 @@ class Dealership
               :address,
               :inventory,
               :inventory_count,
-              :total_value
+              :total_value,
+              :details
   def initialize(name, address)
     @name = name
     @address = address
     @inventory = []
     @inventory_count = 0
     @total_value = 0
+    @details = {"total_value" => @total_value, "address" => @address}
   end
 
   def add_car(car)
     @inventory << car
     @inventory_count += 1
     @total_value += car.total_cost
+    @details["total_value"] += car.total_cost
   end
 
   def has_inventory?
